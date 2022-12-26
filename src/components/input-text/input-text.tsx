89f0ade5ed542e82476a1ec38e5b19a4dce492c5
sergJess <serg.fantasy@yandex.ru>
2022-12-26 22:27:01 +0300

    feat: add trmplate

--- src/components/input-text/input-text.tsx
+++ src/components/input-text/input-text.tsx
@@ -8,14 +8,27 @@ type InputTextState = {
   value: string;
 };
 export class InputText extends React.Component<InputTextProps, InputTextState> {
+  private inputRef = React.createRef<HTMLInputElement>();
   constructor(props: InputTextProps) {
     super(props);
     this.state = {
       value: '',
     };
+    this.changingValue = this.changingValue.bind(this);
   }
-  changingValue(value: string) {}
+  changingValue() {
+    const input = this.inputRef.current;
+    if (input && input.value) {
+      this.setState({ value: input.value });
+    }
+  }
   render() {
-    return <input placeholder={this.props.placeholder} />;
+    return (
+      <input
+        ref={this.inputRef}
+        onChange={this.changingValue}
+        placeholder={this.props.placeholder}
+      />
+    );
   }
 }
